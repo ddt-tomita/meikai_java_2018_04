14b1246a465f189bc860e3a3393345b872f42ff1
ryoko tomita <ddt.tomita@gmail.com>
2018-05-02 19:18:02 +0900

    明解java 演習2-2 変数名の修正

--- E02/E2_2/src/E2_2.java
+++ E02/E2_2/src/E2_2.java
@@ -1,24 +1,27 @@
-/********************************************************************************
- * 演習番号：演習2-2															*
- * 概要：三つのint型変数に値を代入し、合計と平均を求めるプログラムを作成せよ。	*
- * 作成者：富田 涼子															*
- * 作成日：2018/04/25															*
- ********************************************************************************/
+/**
+ * 演習番号：演習2-2
+ * 概要：三つのint型変数に値を代入し、合計と平均を求めるプログラムを作成せよ。
+ * 作成者：富田 涼子
+ * 作成日：2018/04/25
+ * 作成日：2018/05/02
+ */
 
+// 三つのint型変数に値を代入し、合計と平均を求めるクラス
 class Exercise2_2 {
 
+    // 三つのint型変数に値を代入し、合計と平均を求める
     public static void main(String[] args) {
-        int x;        // xはint型の変数
-        int y;        // yはint型の変数
-        int z;        // zはint型の変数
+        int Integer1;        // Integer1はint型の変数
+        int Integer2;        // Integer2はint型の変数
+        int Integer3;        // Integer3はint型の変数
 
-        x = 10;        // xに10を代入
-        y = 20;        // yに20を代入
-        z = 30;        // zに30を代入
+        Integer1 = 10;        // Integer1に10を代入
+        Integer2 = 20;        // Integer2に20を代入
+        Integer3 = 30;        // Integer3に30を代入
 
-        // 変数x,y,zを足して合計の値を画面に出力する
-        System.out.println("合計は" + (x + y + z)  + "です。");
-        // 変数x,y,zを足して3で割って平均の値を画面に出力する
-        System.out.println("平均は" + (x + y + z) / 3 + "です。");
+        // 変数Integer1,Integer2,Integer3を足して合計の値を画面に出力する
+        System.out.println("合計は" + (Integer1 + Integer2 + Integer3)  + "です。");
+        // 変数Integer1,Integer2,Integer3を足して3で割って平均の値を画面に出力する
+        System.out.println("平均は" + (Integer1 + Integer2 + Integer3) / 3 + "です。");
     }
 }
