41a35f6b75417cc668f6abcac21c6a6b3b286d1e
ryoko tomita <ddt.tomita@gmail.com>
2018-05-02 19:37:59 +0900

    明解java 演習2-6 コメントと変数名の修正

--- E02/E2_6/src/E2_6.java
+++ E02/E2_6/src/E2_6.java
@@ -1,15 +1,19 @@
-/********************************************************************************
- * 演習番号：演習2-6（P41）														*
- * 概要： 三角形の底辺と高さを読み込んで、その面積を表示する					*
- * 作成者：富田 涼子															*
- * 作成日：2018/04/27															*
- ********************************************************************************/
+/**
+ * 演習番号：演習2-6（P41）
+ * 概要：三角形の底辺と高さを読み込んで、その面積を表示する
+ * 作成者：富田 涼子
+ * 作成日：2018/04/27
+ * 作成日：2018/05/02
+ */
 import java.util.Scanner;
 
+//三角形の底辺と高さを読み込んで、その面積を表示するクラス
 public class E2_6 {
 
+    //三角形の底辺と高さを読み込んで、その面積を表示する
     public static void main(String[] args) {
-        Scanner stdIn = new Scanner(System.in);
+        // 標準入力ストリームのインスタンス化
+        Scanner StandardInput = new Scanner(System.in);
 
         // これから表示する内容の説明を画面出力しています
         System.out.println("三角形の面積を求めます。");
@@ -17,12 +21,12 @@ public class E2_6 {
         // 底辺の値の入力を促す
         System.out.println("底辺：");
         // 変数bottomに、画面から入力された底辺の値を格納する
-        double bottom = stdIn.nextDouble();
+        double bottom = StandardInput.nextDouble();
 
         // 高さの値の入力を促す
         System.out.println("高さ：");
         // 変数heightに、画面から入力された高さの値を格納する
-        double height = stdIn.nextDouble();
+        double height = StandardInput.nextDouble();
 
         // 入力された底辺と高さを元に、三角形の面積を画面に出力する（底辺×高さ÷２）
         System.out.println("面積は" + (bottom * height / 2) + "です。");
