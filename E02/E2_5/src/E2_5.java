422b7fb4b2980162806bd0920b2bbfa831a954f6
ryoko tomita <ddt.tomita@gmail.com>
2018-05-02 19:35:29 +0900

    明解java 演習2-5 コメントの修正

--- E02/E2_5/src/E2_5.java
+++ E02/E2_5/src/E2_5.java
@@ -1,22 +1,28 @@
-/********************************************************************************
- * 演習番号：演習2-5（P41）														*
- * 概要： 二つの実数値を読み込み、その和と平均を求めて表示する					*
- * 作成者：富田 涼子															*
- * 作成日：2018/04/27															*
- ********************************************************************************/
+/**
+ * 演習番号：演習2-5（P41）
+ * 概要：二つの実数値を読み込み、その和と平均を求めて表示する
+ * 作成者：富田 涼子
+ * 作成日：2018/04/27
+ * 作成日：2018/05/02
+ */
 
 import java.util.Scanner;
 
+// 二つの実数値を読み込み、その和と平均を求めて表示するクラス
 class E2_5 {
 
+    // 二つの実数値を読み込み、その和と平均を求めて表示する
     public static void main(String[] args) {
-        Scanner stdIn = new Scanner(System.in);
+        // 標準入力ストリームのインスタンス化
+        Scanner StandardInput = new Scanner(System.in);
 
-        System.out.println("xの値：");		// xの値の入力を促す
-        double x = stdIn.nextDouble();		// xに実数値を読み込む
+        // xの値の入力を促す
+        System.out.println("xの値：");
+        double x = StandardInput.nextDouble();  // xに実数値を読み込む
 
-        System.out.println("yの値：");		// yの値の入力を促す
-        double y = stdIn.nextDouble();		// yに実数値を読み込む
+        // yの値の入力を促す
+        System.out.println("yの値：");
+        double y = StandardInput.nextDouble();  // yに実数値を読み込む
 
         // 画面から入力されたxの値とyの値から合計値を表示する
         System.out.println("合計は" + (x + y)  + "です。");
