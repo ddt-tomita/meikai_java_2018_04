25f14f7e8e6f4511b5273b0f13b905cc377e3c0a
ryoko tomita <ddt.tomita@gmail.com>
2018-05-11 19:50:45 +0900

    明解java 演習2-2 変数名の修正２

--- E02/E2_2/src/E2_2.java
+++ E02/E2_2/src/E2_2.java
@@ -11,17 +11,17 @@ class Exercise2_2 {
 
     // 三つのint型変数に値を代入し、合計と平均を求める
     public static void main(String[] args) {
-        int Integer1;        // Integer1はint型の変数
-        int Integer2;        // Integer2はint型の変数
-        int Integer3;        // Integer3はint型の変数
+        int int1st;        // int1stはint型の変数
+        int int2nd;        // int2ndはint型の変数
+        int int3rd;        // int3rdはint型の変数
 
-        Integer1 = 10;        // Integer1に10を代入
-        Integer2 = 20;        // Integer2に20を代入
-        Integer3 = 30;        // Integer3に30を代入
+        int1st = 10;        // int1stに10を代入
+        int2nd = 20;        // int2ndに20を代入
+        int3rd = 30;        // int3rdに30を代入
 
-        // 変数Integer1,Integer2,Integer3を足して合計の値を画面に出力する
-        System.out.println("合計は" + (Integer1 + Integer2 + Integer3)  + "です。");
-        // 変数Integer1,Integer2,Integer3を足して3で割って平均の値を画面に出力する
-        System.out.println("平均は" + (Integer1 + Integer2 + Integer3) / 3 + "です。");
+        // 変数int1st,int2nd,int3rdを足して合計の値を画面に出力する
+        System.out.println("合計は" + (int1st + int2nd + int3rd)  + "です。");
+        // 変数int1st,int2nd,int3rdを足して3で割って平均の値を画面に出力する
+        System.out.println("平均は" + (int1st + int2nd + int3rd) / 3 + "です。");
     }
 }
