b05f916776f91248e44f5838462daa89b0a08b6f
ryoko tomita <ddt.tomita@gmail.com>
2018-06-18 20:56:27 +0900

    明解java 演習4-6-1 初回コミット

---
+++ E03/E4/src/e4_6/E4_6_1.java
@@ -0,0 +1,38 @@
+/**
+ * 演習番号：演習4-6（P109）
+ * 概要：読み込んだ値が1未満であれば改行文字を出力しないようにList4-7を書き換えたプログラムをそれぞれ作成せよ。
+ * 作成者：富田 涼子
+ * 作成日：2018/06/18
+ * 修正日：2018/06/18
+ */
+package e4_6;
+import java.util.Scanner;
+
+// 読み込んだ個数だけ*を表示するクラス
+public class E4_6_1 {
+    // 読み込んだ個数だけ*を表示する
+    public static void main(String[] args) {
+        // 標準入力ストリームのインスタンス化をする
+        Scanner standardInput = new Scanner(System.in);
+
+        // 何個*を表示するかをユーザーに入力してもらう
+        System.out.print("何個*を表示しますか：");
+        // 画面から入力された値をinputValueに格納する
+        int inputValue = standardInput.nextInt();
+
+        // 画面に出力した*の数を数えるための変数
+        int countValue = 0;
+        // countDownValueの値がinputValueの値より小さいの間、繰り返し処理を行う。
+        while (countValue < inputValue) {
+            // 画面に*を表示する
+            System.out.print('*');
+            // 画面出力処理実行後、countValueの値をインクリメントする
+            countValue++;
+            // countValueの値とinputValueの値が一致したとき(ループを抜ける前に)、改行文字を出力する
+            if (countValue == inputValue) {
+                // 画面へ改行文字の出力を行う
+                System.out.println();
+            }
+        }
+    }
+}
