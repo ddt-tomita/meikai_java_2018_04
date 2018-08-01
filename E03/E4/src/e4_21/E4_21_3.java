e11049b1ab9b641fbb513d3a8b1e2655ca605ef6
ryoko tomita <ddt.tomita@gmail.com>
2018-08-01 20:00:01 +0900

    明解java 演習4-21 初回コミット

--- E03/E4/src/e4_21/E4_21_3.java
+++ E03/E4/src/e4_21/E4_21_3.java
@@ -4,15 +4,15 @@
  *       三角形を表示するプログラムを作成せよ。
  *       ※直角が右下側
  * 作成者：富田 涼子
- * 作成日：2018/07/25
+ * 作成日：2018/08/01
  * 修正日：2018/08/01
  */
 package e4_21;
 import java.util.Scanner;
 
-// 直角が右下側にある直角三角形を表示するクラス
-public class E4_21_2 {
-    // 直角が右下側にある直角三角形を表示する
+// 直角が右上側にある直角三角形を表示するクラス
+public class E4_21_3 {
+    // 直角が右上側にある直角三角形を表示する
     public static void main(String[] args) {
         // 標準入力ストリームのインスタンス化をする
         Scanner standardInput = new Scanner(System.in);
@@ -25,15 +25,15 @@ public class E4_21_2 {
         // 画面から入力された値が０以下であれば繰り返し処理を行う
         while (inputValue <= valueZero) {
             // プログラムの説明を行います。
-            System.out.print("右下直角の三角形を表示します。");
+            System.out.print("右上直角の三角形を表示します。");
             // 段数の入力を促す
             System.out.print("段数は：");
             // 画面から入力された値をinputValueに格納する
             inputValue = standardInput.nextInt();
         }
 
-        // 直角三角形の幅を管理する変数。初期値に入力値を入れる。
-        int widthCountInt = inputValue;
+        // 直角三角形の幅を管理する変数。
+        int widthCountInt = 1;
 
         // 直角三角形の高さを管理するループ文。入力した値の数だけ繰り返す。
         for ( int heightInt = 1; heightInt <= inputValue; heightInt++) {
@@ -50,8 +50,8 @@ public class E4_21_2 {
             }
             // 改行を行う
             System.out.println("");
-            // 1行出力したら、次の幅を管理するループでは1マイナスした数をループする
-            widthCountInt--;
+            // 1行出力したら、次の幅を管理するループでは1プラスした数をループする
+            widthCountInt++;
         }
     }
 }
