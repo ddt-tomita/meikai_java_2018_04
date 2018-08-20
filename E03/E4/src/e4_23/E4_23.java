a1ff9a1ac4b7c98a7cc5212ab019f13584742084
ryoko tomita <ddt.tomita@gmail.com>
2018-08-20 20:25:58 +0900

    明解java 演習4-23 初回コミット

--- E03/E4/src/e4_23/E4_23.java
+++ E03/E4/src/e4_23/E4_23.java
@@ -3,7 +3,7 @@
  * 概要：n段のピラミッドを表示するプログラムを作成せよ
  * 作成者：富田 涼子
  * 作成日：2018/08/01
- * 修正日：2018/08/01
+ * 修正日：2018/08/20
  */
 package e4_23;
 
@@ -19,47 +19,50 @@ public class E4_23 {
         // 画面から入力された値を格納する変数
         int inputValue = 0;
         // 入力された値を制限するための変数
-        int valueZero = 0;
+        final int VALUE_ZERO = 0;
 
-        // 画面から入力された値が０以下であれば繰り返し処理を行う
-        while (inputValue <= valueZero) {
+        // 以下、繰り返し処理
+        do {
             // プログラムの説明を行います。
             System.out.println("ピラミッドを表示します。");
             // 段数の入力を促す
             System.out.print("段数は：");
             // 画面から入力された値をinputValueに格納する
             inputValue = standardInput.nextInt();
-        }
+        // 画面から入力された値が０以上であれば終了
+        } while (inputValue <= VALUE_ZERO);
 
-        // 現在の行数をあらわす変数
-        int nowRow = 1;
+        // 現在のピラミッドの行数をあらわす変数
+        int pyramidNowRow = 1;
         // ピラミッドの幅をあらわす変数
-        int widthCountInt = 0;
+        int pyramidWidthCountInt = 0;
         // スペースの数をあらわす変数
         int spaceCountInt = inputValue;
-        //
-        int a = nowRow % 10;
+        // ピラミッドの段数をあらわす変数
+        int pyramidNumberOfStages = pyramidNowRow % 10;
 
         // ピラミッドの高さを管理するループ文。入力した値の数だけ繰り返す。
         for (int heightInt = 1; heightInt <= inputValue; heightInt++) {
             // ピラミッドの幅をここで決める
-            widthCountInt = (nowRow - 1) * 2 + 1;
+            pyramidWidthCountInt = (pyramidNowRow - 1) * 2 + 1;
             // ピラミッドの左側のスペース部を管理するループ文。
             for (int spaceInt = 1; spaceInt <= spaceCountInt; spaceInt++) {
                 // 画面にスペースを表示する
                 System.out.print(' ');
             }
             // ピラミッド本体を管理するループ文
-            for (int widthInt = 0; widthInt < widthCountInt; widthInt++) {
-                // 画面に*を表示する
-                System.out.print(a);
+            for (int widthInt = 0; widthInt < pyramidWidthCountInt; widthInt++) {
+                // 画面に段数の数字を表示する
+                System.out.print(pyramidNumberOfStages);
             }
             // 改行を行う
             System.out.println("");
             // 1行出力したら、スペース部を1減らす
             spaceCountInt--;
-            // 次の行数を設定する
-            nowRow++;
+            // 次のピラミッドの段数を設定する
+            pyramidNumberOfStages++;
+            // 次のピラミッドの行数を設定する
+            pyramidNowRow++;
         }
     }
 }
