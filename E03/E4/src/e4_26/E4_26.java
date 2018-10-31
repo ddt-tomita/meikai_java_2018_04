f6141ece62852d8be69730f19a78e7ed66112378
LAPTOP-U4U9QT1U\tomita <ddt.tomita@gmail.com>
2018-10-31 21:05:44 +0900

    明解java 演習4-26  0除算の考慮

--- E03/E4/src/e4_26/E4_26.java
+++ E03/E4/src/e4_26/E4_26.java
@@ -4,7 +4,7 @@
  *       なお、読み込んだ負の数の個数は平均を求める分母から除外すること。
  * 作成者：富田 涼子
  * 作成日：2018/09/17
- * 修正日：2018/10/29
+ * 修正日：2018/10/31
  */
 package e4_26;
 
@@ -58,8 +58,15 @@ public class E4_26 {
             // 平均値を算出するための分母をインクリメントしていく
             divisionValue++;
         }
-        // 合計値を入力回数(負の値が入力された回数は除く)で割って平均値を求める
-        aveValue = sumValue / divisionValue;
+		// 入力回数が０回以上の場合
+		if(divisionValue > VALUE_ZERO) {
+	        // 合計値を入力回数(負の値が入力された回数は除く)で割って平均値を求める
+			aveValue = sumValue / divisionValue;
+		// 入力回数が０回の場合(負の値のみ入力された場合)
+		} else {
+			// 平均値は０となる
+			aveValue = VALUE_ZERO;
+		}
 
         // 入力された値の合計を出力する
         System.out.println("合計は" + sumValue + "です。");
