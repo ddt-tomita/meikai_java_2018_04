02116982d3c85a9e6b9502e5f9233e170903db9f
LAPTOP-U4U9QT1U\tomita <ddt.tomita@gmail.com>
2019-03-06 21:29:31 +0900

    演習6-5 ◇当日行った内容  出力部分のソースをコーディングし、演習6-5を完成させる ◇演習6-5の全体進捗率  100％ ◇次回todo  演習6-6までのテキストを読み、演習6-6にとりかかる。

--- E03/E6/src/e6_5/E6_5.java
+++ E03/E6/src/e6_5/E6_5.java
@@ -4,7 +4,7 @@
  * 		 表示の形式は、初期化子と同じ形式、すなわち、各要素の値をコンマで区切って{}で囲んだ形式とすること。
  * 作成者：富田 涼子
  * 作成日：2018/12/03
- * 修正日：2018/12/03
+ * 修正日：2019/03/06
  */
 package e6_5;
 
@@ -26,14 +26,31 @@ public class E6_5 {
 		// 画面に表示する配列の要素数を決定する。
 		int[] inputValue = new int[ELEMENT_COUNT];
 
+		// 最初に指定された要素数分、利用者に値を入力してもらう。
 		for (int countValue = 0; countValue < ELEMENT_COUNT; countValue++) {
-			// 要素数の入力を促す文字列を画面に表示する
+			// 要素数の値の入力を促す文字列を画面に表示する
 			System.out.print("outputValue["+countValue+"] = ");
-			//
+			// 利用者が画面から入力した値を配列に格納する
 			inputValue[countValue] = standardInput.nextInt();
 		}
 
-		//
-		int[] outputValue = {};
+		// 画面出力用の変数
+		StringBuffer outputValue = new StringBuffer("outputValue =｛");
+		// 最終ループ判定用変数
+		final int LAST_LOOP_COUNT = ELEMENT_COUNT - 1;
+		// 配列の要素数分ループする
+		for (int i = 0; i < ELEMENT_COUNT; i++) {
+			// 配列の値を画面出力用変数に取り出していく
+			outputValue.append(inputValue[i]);
+			// 最終ループではない場合
+			if (i != LAST_LOOP_COUNT) {
+				// カンマを結合する
+				outputValue.append("，");
+			}
+		}
+		// 最後のカッコを結合する
+		 outputValue.append("｝");
+		// 画面に結合した文字列を出力する
+		System.out.println(outputValue);
 	}
 }
